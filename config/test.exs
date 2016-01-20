9148957749af457f96a7306bc262220d046da0f6
Mickey <poverwhelming@gmail.com>
2016-01-20 15:45:19 +0800

    completed part 2

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,6 @@ config :logger, level: :warn
 # Configure your database
 config :seat_saver, SeatSaver.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "seat_saver_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
